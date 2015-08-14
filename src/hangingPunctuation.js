99b1a7215d10f8eebcd39189beb28652418350c4
David Merfield <dmerfield@gmail.com>
2015-08-13 18:57:33 -0700

    updated hanging punctuation module

--- src/hangingPunctuation.js
+++ src/hangingPunctuation.js
@@ -5,8 +5,10 @@ module.exports = function smallCaps (html, options) {
 
 
     var hangMe = ['&quot;', '"', "“", "„", "”", "&ldquo;", "&OpenCurlyDoubleQuote;", "&#8220;", "&#x0201C;", "&rdquor;", "&rdquo;", '&CloseCurlyDoubleQuote;', '&#8221;', '&ldquor;', '&bdquo;', '&#8222;'];
-    
-    html = eachTextNode(html, function(text, node){ 
+
+    var alignMe = "CcOoYTAVvWwY".split('');
+
+    html = eachTextNode(html, function(text, node){
 
       // Remove consecutive double spaces then create
       // array of distinct words.
@@ -14,13 +16,26 @@ module.exports = function smallCaps (html, options) {
 
       for (var i in words) {
 
+        for (var j in alignMe) {
+
+          var align = alignMe[j];
+
+          if (words[i].slice(0,align.length) === align) {
+            words[i] = '<aria class="hang-' + align +'">' + align + '</aria>' + words[i].slice(align.length);
+
+            if (words[(i-1)]) {
+                words[(i-1)] = words[(i-1)] + '<span class="space-' + align +'"></span>';
+            }
+          }
+        }
+
         for (var j in hangMe) {
-          
+
           var punctuation = hangMe[j];
 
           if (words[i].slice(0,punctuation.length) === punctuation) {
-              
-              words[i] = '<span class="hang-me">' + punctuation + '</span>' + words[i].slice(punctuation.length);
+
+              words[i] = '<aria class="hang-me">' + punctuation + '</aria>' + words[i].slice(punctuation.length);
 
               if (words[(i-1)]) {
                   words[(i-1)] = words[(i-1)] + '<span class="space-me"></span>';
@@ -29,7 +44,7 @@ module.exports = function smallCaps (html, options) {
         }
       }
 
-      text = words.join(' '); 
+      text = words.join(' ');
 
       return text;
     });
