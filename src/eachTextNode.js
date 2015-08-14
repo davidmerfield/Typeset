34f7d598bf973c08625cd775bff7774491c59cb4
David Merfield <dmerfield@gmail.com>
2015-08-13 18:57:40 -0700

    updated each text node

--- src/eachTextNode.js
+++ src/eachTextNode.js
@@ -4,22 +4,23 @@ var cheerio = require('cheerio');
 // doThis accepts a text string of a text node's content
 // and returns the modified string.
 
+var IGNORE = 'head, code, pre, script, style, .ts-ignore';
+
 module.exports = function forEachTextNode (html, doThis) {
-    
+
     var $ = cheerio.load(html, {decodeEntities: false});
 
     $(":root").each(function(){
-        
+
         findTextNodes(this);
 
         function findTextNodes(node) {
-            
+
+            if ($(node).is(IGNORE)) return false;
+
+            if (IGNORE.indexOf() > -1) return false;
+
             $(node).contents().each(function(){
-                
-                // We want to ignore any text in these
-                // elements for obvious reasons
-                if ($(node).parents().filter('head, code, pre, script, style').length) 
-                    return false;
 
                 var childNode = $(this)[0];
 
@@ -27,13 +28,12 @@ module.exports = function forEachTextNode (html, doThis) {
                 // apply the function which transforms
                 // its text content (childNode.data)
                 if (childNode.type === 'text') {
-        
                     childNode.data = doThis(childNode.data, childNode);
                 } else {
                     findTextNodes(childNode, doThis);
                 }
             });
-        
+
         }
 
     });
