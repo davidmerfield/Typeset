9872f7438f63a4ab4c33c99a8e539e5d834ac4c1
faheem <faheem@faheempatel.com>
2015-08-18 06:32:46 +0100

    Improve CLI

--- src/cmd.js
+++ src/cmd.js
@@ -1,22 +1,45 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var program = require('commander');
 var typeset = require('./index.js');
 
-var userArgs = process.argv.slice(2);
-var inputFile = userArgs[0];
-var outputFile = userArgs[1];
+// CLI
+program
+  .version(require('../package.json').version)
+  .usage('<entry file> [options]')
+  .option('-o, --outfile <output file>', 'write Typeset output to this file (if unspecified, Typeset will print to stdout)')
+  .option('-i, --ignore <string containing selectors to ignore>', 'string of CSS selector(s) to ignore')
+  .option('-O, --only <string containing selectors to parse>', 'string of CSS selector(s) to exclusively apply typeset to')
+  .parse(process.argv);
 
-if (userArgs.length < 2) {
-  throw new Error('Not enough args');
+if (program.args.length == 0) {
+  // display help and exit
+  program.help();
 }
+
+var inputFile = program.args[0];
+var outputFile = program.outfile || false;
+
+var options = {
+  ignore: program.ignore || '',
+  only: program.only || '',
+};
 
 fs.readFile(inputFile, function(err, data) {
 
   if (err) throw err;
 
-  var outputHTML = typeset(data);
-  fs.writeFile(outputFile, outputHTML, function(err) {
-    if (err) throw err;
-  });
+  var outputHTML = typeset(data, options);
+
+  if (outputFile) {
+
+    fs.writeFile(outputFile, outputHTML, function(err) {
+      if (err) throw err;
+    });
+
+  } else {
+    // print output to stdout by default
+    console.log(outputHTML);
+  }
 
 });
