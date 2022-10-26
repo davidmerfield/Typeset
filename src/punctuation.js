d145107616d5591bd673b4bf81f4d6378e8fc193
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2022-10-26 18:01:32 -0400

    Added rules and tests for N Dash number ranges

--- src/punctuation.js
+++ src/punctuation.js
@@ -1,7 +1,14 @@
 module.exports = (text) => {
 
+  // N Dash
+  // https://en.wikipedia.org/wiki/Dash#En_dash
+  // This is before M Dash because we want to handle number ranges as a special case before the more general fixes below.
+  text = text.replace(/(\d+)\s?-\s?(\d+)/g, "$1&thinsp;&ndash;&thinsp;$2");
+  text = text.replace(/(\d+)\s?–\s?(\d+)/g, "$1&thinsp;&ndash;&thinsp;$2");
+  text = text.replace(/(\d+)\s?&mdash;\s?(\d+)/g, "$1&thinsp;&ndash;&thinsp;$2");
+
   // M Dash
-  // https://en.wikipedia.org/wiki/Dash
+  // https://en.wikipedia.org/wiki/Dash#Em_dash
   text = text.replace(/--/g, "–");
   text = text.replace(/ – /g, "&thinsp;&mdash;&thinsp;");
 
